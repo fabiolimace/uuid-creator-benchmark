955fa36866826d66736f1c5dee879e81880dd233
Fabio Lima <fabiolimace@gmail.com>
2020-04-19 00:23:18 -0300

    Add more methods
    Added new methods to benchmark ULIDs and TSIDs

--- src/main/java/com/github/f4b6a3/uuid/benchmark/AverageTime.java
+++ src/main/java/com/github/f4b6a3/uuid/benchmark/AverageTime.java
@@ -1,6 +1,5 @@
 package com.github.f4b6a3.uuid.benchmark;
 
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
@@ -12,7 +11,6 @@ import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Threads;
 import org.openjdk.jmh.annotations.Warmup;
@@ -21,51 +19,23 @@ import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
+import com.github.f4b6a3.tsid.TsidCreator;
+import com.github.f4b6a3.tsid.exception.TsidCreatorException;
+import com.github.f4b6a3.ulid.UlidCreator;
+import com.github.f4b6a3.ulid.exception.UlidCreatorException;
 import com.github.f4b6a3.uuid.UuidCreator;
 import com.github.f4b6a3.uuid.exception.UuidCreatorException;
-import com.github.f4b6a3.uuid.factory.CombGuidCreator;
-import com.github.f4b6a3.uuid.factory.NameBasedMd5UuidCreator;
-import com.github.f4b6a3.uuid.factory.NameBasedSha1UuidCreator;
-import com.github.f4b6a3.uuid.factory.RandomUuidCreator;
-import com.github.f4b6a3.uuid.factory.SequentialUuidCreator;
-import com.github.f4b6a3.uuid.factory.TimeBasedUuidCreator;
-import com.github.f4b6a3.uuid.factory.UlidBasedGuidCreator;
 
 @Threads(1)
 @Fork(1)
 @State(Scope.Thread)
-@Warmup(iterations = 1)
+@Warmup(iterations = 3)
 @Measurement(iterations = 5)
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 public class AverageTime {
 
 	private byte[] bytes = "John Smith".getBytes(StandardCharsets.UTF_8);
-
-	private RandomUuidCreator randomCreator;
-	private RandomUuidCreator fastRandomCreator;
-	private NameBasedMd5UuidCreator nameBasedMd5Creator;
-	private NameBasedSha1UuidCreator nameBasedSha1Creator;
-	private SequentialUuidCreator sequentialCreator;
-	private TimeBasedUuidCreator timeBasedCreator;
-	private SequentialUuidCreator sequentialMacCreator;
-	private TimeBasedUuidCreator timeBasedMacCreator;
-	private CombGuidCreator combCreator;
-	private UlidBasedGuidCreator ulidBasedCreator;
-
-	@Setup
-	public void setUp() throws IOException {
-		randomCreator = UuidCreator.getRandomCreator();
-		fastRandomCreator = UuidCreator.getFastRandomCreator();
-		nameBasedMd5Creator = UuidCreator.getNameBasedMd5Creator();
-		nameBasedSha1Creator = UuidCreator.getNameBasedSha1Creator();
-		sequentialCreator = UuidCreator.getSequentialCreator();
-		timeBasedCreator = UuidCreator.getTimeBasedCreator();
-		sequentialMacCreator = UuidCreator.getSequentialCreator().withHardwareAddressNodeIdentifier();
-		timeBasedMacCreator = UuidCreator.getTimeBasedCreator().withHardwareAddressNodeIdentifier();
-		combCreator = UuidCreator.getCombCreator();
-		ulidBasedCreator = UuidCreator.getUlidBasedCreator();
-	}
 
 	// Java UUID
 
@@ -77,7 +47,7 @@ public class AverageTime {
 	// EAIO
 
 	@Benchmark
-	public com.eaio.uuid.UUID EAIO_TimeBasedWithMac() {
+	public com.eaio.uuid.UUID Eaio_TimeBased() {
 		return new com.eaio.uuid.UUID();
 	}
 
@@ -86,67 +56,84 @@ public class AverageTime {
 	@Benchmark
 	public UUID UuidCreator_TimeBased() {
 		try {
-			return timeBasedCreator.create();
+			return UuidCreator.getTimeBased();
 		} catch (UuidCreatorException e) {
 			return null;
 		}
 	}
 
 	@Benchmark
-	public UUID UuidCreator_TimeBasedWithMac() {
+	public UUID UuidCreator_TimeOrdered() {
 		try {
-			return timeBasedMacCreator.create();
+			return UuidCreator.getTimeOrdered();
 		} catch (UuidCreatorException e) {
 			return null;
 		}
 	}
 
 	@Benchmark
-	public UUID UuidCreator_Sequential() {
-		try {
-			return sequentialCreator.create();
-		} catch (UuidCreatorException e) {
-			return null;
-		}
+	public UUID UuidCreator_RandomBased() {
+		return UuidCreator.getRandomBased();
 	}
 
 	@Benchmark
-	public UUID UuidCreator_SequentialWithMac() {
-		try {
-			return sequentialMacCreator.create();
-		} catch (UuidCreatorException e) {
-			return null;
-		}
-	}
-
-	@Benchmark
-	public UUID UuidCreator_UlidBasedGuid() {
-		return ulidBasedCreator.create();
-	}
-
-	@Benchmark
-	public UUID UuidCreator_CombGuid() {
-		return combCreator.create();
-	}
-
-	@Benchmark
-	public UUID UuidCreator_Random() {
-		return randomCreator.create();
-	}
-
-	@Benchmark
-	public UUID UuidCreator_FastRandom() {
-		return fastRandomCreator.create();
+	public UUID UuidCreator_FastRandomBased() {
+		return UuidCreator.getFastRandomBased();
 	}
 
 	@Benchmark
 	public UUID UuidCreator_NameBasedMd5() {
-		return nameBasedMd5Creator.create(bytes);
+		return UuidCreator.getNameBasedMd5(bytes);
 	}
 
 	@Benchmark
 	public UUID UuidCreator_NameBasedSha1() {
-		return nameBasedSha1Creator.create(bytes);
+		return UuidCreator.getNameBasedSha1(bytes);
+	}
+
+	@Benchmark
+	public UUID UuidCreator_CombGuid() {
+		return UuidCreator.getCombGuid();
+	}
+
+	// ULID Creator
+
+	@Benchmark
+	public UUID UlidCreator_Ulid() {
+		try {
+			return UlidCreator.getUlid();
+		} catch (UlidCreatorException e) {
+			return null;
+		}
+	}
+
+	@Benchmark
+	public String UlidCreator_UlidString() {
+		try {
+			return UlidCreator.getUlidString();
+		} catch (UlidCreatorException e) {
+			return null;
+		}
+	}
+
+	// TSID Creator
+
+	@Benchmark
+	public long TsidCreator_Tsid() {
+		try {
+			return TsidCreator.getTsid();
+		} catch (TsidCreatorException e) {
+			return 0;
+		}
+	}
+
+	@Benchmark
+	public String TsidCreator_TsidString() {
+		try {
+			return TsidCreator.getTsidString();
+		} catch (TsidCreatorException e) {
+			return null;
+		}
 	}
 
 	public static void main(String[] args) throws RunnerException {
